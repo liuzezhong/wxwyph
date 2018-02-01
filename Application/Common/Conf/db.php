c2eb277b36f178edf5cb1096068b1412e95b77c9
liuzezhong <zhongliuze@gmail.com>
2018-02-01 09:55:10 +0800

    2017-02-01

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -6,7 +6,7 @@
  * Time: 20:32
  */
 
-/*return array(
+return array(
     //数据库配置
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => 'localhost', // 服务器地址
@@ -17,9 +17,9 @@
     'DB_PREFIX' => 'bj_', // 数据库表前缀
     'DB_CHARSET' => 'utf8mb4', // 字符集
     'DB_DEBUG' => true, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
-);*/
+);
 
-return array(
+/*return array(
     //数据库配置
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => 'server-xianshikeji.mysql.rds.aliyuncs.com', // 服务器地址
@@ -30,4 +30,4 @@ return array(
     'DB_PREFIX' => 'bj_', // 数据库表前缀
     'DB_CHARSET' => 'utf8mb4', // 字符集
     'DB_DEBUG' => true, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
-);
+);*/
